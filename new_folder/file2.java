ecc5aaf064d155118691cf1a544b45ad404ce349
NarenRathore <51296725+NarenRathore@users.noreply.github.com>
2021-06-18 16:59:11 +0530

    2nd file is created

---
+++ new_folder/file2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+    //this method takes array of intergers as input as return the average 
+    public static double Average(int[] arr){
+        double sum=0;
+        for(int i:arr)  //iterate over the intergers in array
+            sum+=i;     //add them to sum
+        //divide sum by number of elements in array to get average
+        sum=sum/arr.length;
+        //return sum 
+        return sum;
+    }
+}
